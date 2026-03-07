6afab2c036814051b7abe962ffb2a8347a6b2e83
Tinkudev <188956193+Chauhanrajk007@users.noreply.github.com>
2026-03-07 13:06:57 +0530

    Create create-order.js

---
+++ api/create-order.js
@@ -0,0 +1,32 @@
+import Razorpay from "razorpay";
+
+export default async function handler(req, res) {
+
+const razorpay = new Razorpay({
+key_id: process.env.RAZORPAY_KEY_ID,
+key_secret: process.env.RAZORPAY_KEY_SECRET
+});
+
+const options = {
+
+amount: 100,
+
+currency: "INR",
+
+receipt: "receipt_1"
+
+};
+
+try {
+
+const order = await razorpay.orders.create(options);
+
+res.status(200).json(order);
+
+} catch (error) {
+
+res.status(500).json({ error: "Order creation failed" });
+
+}
+
+}
