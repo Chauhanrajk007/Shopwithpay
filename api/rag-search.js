8fa056d4bce5e1471fa788a210722ac762ef582d
Tinkudev <188956193+Chauhanrajk007@users.noreply.github.com>
2026-03-09 14:04:08 +0530

    Update rag-search.js

--- api/rag-search.js
+++ api/rag-search.js
@@ -1,28 +1,33 @@
 import { MongoClient } from "mongodb"
 
-export default async function handler(req, res) {
+export default async function handler(req,res){
 
-try {
+try{
 
-const body = req.body || {}
-const query = body.query
+let body = req.body
 
-if (!query) {
-return res.status(400).json({ error: "Query missing" })
+if(typeof body === "string"){
+body = JSON.parse(body)
 }
 
-/* Create embedding for query */
+const query = body?.query
+
+if(!query){
+return res.status(400).json({error:"Query missing"})
+}
+
+/* create embedding */
 
 const response = await fetch(
 `https://generativelanguage.googleapis.com/v1beta/models/gemini-embedding-001:embedContent?key=${process.env.GEMINI_API_KEY}`,
 {
-method: "POST",
-headers: {
-"Content-Type": "application/json"
+method:"POST",
+headers:{
+"Content-Type":"application/json"
 },
-body: JSON.stringify({
-content: {
-parts: [{ text: query }]
+body:JSON.stringify({
+content:{
+parts:[{text:query}]
 }
 })
 }
@@ -30,13 +35,11 @@ parts: [{ text: query }]
 
 const embedData = await response.json()
 
-if (!embedData.embedding || !embedData.embedding.values) {
-return res.status(500).json({ error: "Embedding failed", embedData })
+if(!embedData.embedding){
+return res.status(500).json({error:"Embedding failed",embedData})
 }
 
 const queryVector = embedData.embedding.values
-
-/* Connect MongoDB */
 
 const client = new MongoClient(process.env.MONGODB_URI)
 
@@ -44,29 +47,25 @@ await client.connect()
 
 const db = client.db("ragDB")
 
-/* Vector search */
-
 const results = await db.collection("products").aggregate([
 {
-$vectorSearch: {
-index: "product_vector",
-path: "embedding",
-queryVector: queryVector,
-numCandidates: 100,
-limit: 5
+$vectorSearch:{
+index:"product_vector",
+path:"embedding",
+queryVector:queryVector,
+numCandidates:20,
+limit:5
 }
 }
 ]).toArray()
 
-return res.json(results)
+res.json(results)
 
-} catch (err) {
+}catch(err){
 
 console.error(err)
 
-return res.status(500).json({
-error: err.message
-})
+res.status(500).json({error:err.message})
 
 }
 
