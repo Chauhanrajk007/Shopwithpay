01469aef0833fb914670c5834b3b16b6a059db74
Tinkudev <188956193+Chauhanrajk007@users.noreply.github.com>
2026-03-09 11:07:40 +0530

    Update rag-search.js

--- api/rag-search.js
+++ api/rag-search.js
@@ -3,13 +3,21 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 
 export default async function handler(req,res){
 
+try{
+
 const {query} = req.body
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
-const model = genAI.getGenerativeModel({ model:"text-embedding-004" })
+const model = genAI.getGenerativeModel({
+model:"embedding-001"
+})
 
-const embed = await model.embedContent(query)
+const embed = await model.embedContent({
+content:{
+parts:[{text:query}]
+}
+})
 
 const queryVector = embed.embedding.values
 
@@ -19,13 +27,13 @@ await client.connect()
 
 const db = client.db("ragDB")
 
-const results = await db.collection("documents").aggregate([
+const results = await db.collection("products").aggregate([
 
 {
 $vectorSearch:{
 index:"product_vector",
 path:"embedding",
-queryVector,
+queryVector:queryVector,
 numCandidates:100,
 limit:5
 }
@@ -34,5 +42,13 @@ limit:5
 ]).toArray()
 
 res.json(results)
+
+}catch(err){
+
+console.error(err)
+
+res.status(500).json({error:err.message})
+
+}
 
 }
