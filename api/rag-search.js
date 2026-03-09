726991f80fad56bd05fbeb0ceed6948edc2b9aba
Tinkudev <188956193+Chauhanrajk007@users.noreply.github.com>
2026-03-09 12:59:34 +0530

    Update rag-search.js

--- api/rag-search.js
+++ api/rag-search.js
@@ -6,17 +6,20 @@ try{
 
 const {query} = req.body
 
+/* create embedding for query */
+
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1/models/text-embedding-004:embedContent?key=${process.env.GEMINI_API_KEY}`,
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-embedding-001:embedContent?key=${process.env.GEMINI_API_KEY}`,
 {
 method:"POST",
 headers:{
 "Content-Type":"application/json"
 },
 body:JSON.stringify({
-model:"models/text-embedding-004",
 content:{
-parts:[{text:query}]
+parts:[
+{ text: query }
+]
 }
 })
 }
@@ -24,13 +27,25 @@ parts:[{text:query}]
 
 const data = await response.json()
 
+if(!data.embedding || !data.embedding.values){
+
+throw new Error(
+"Embedding API failed → " + JSON.stringify(data)
+)
+
+}
+
 const queryVector = data.embedding.values
+
+/* connect MongoDB */
 
 const client = new MongoClient(process.env.MONGODB_URI)
 
 await client.connect()
 
 const db = client.db("ragDB")
+
+/* vector search */
 
 const results = await db.collection("products").aggregate([
 {
